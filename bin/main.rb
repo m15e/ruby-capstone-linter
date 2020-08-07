912337cb0c89b9791851cc8f9903e98e2f9730e4
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-07 23:43:07 +0800

    ADD code finds all .css files in directory with option specify files to ignore

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
 
-puts 'hi from main.rb'
+# get input
+
+in_args = ARGV
+
+
+files = Dir.glob('**/**.css')  
+
+if files.empty?
+  puts 'File ending with .css does not exist in this project.'
+  exit
+end
+
+if in_args[0] == 'ignore'
+  scrap = in_args.select { |file_name| file_name.end_with?('.css')  }
+  scrap.each { |file| files.delete(file)  }
+end
+
+p files
