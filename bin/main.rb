c31b8d07105318e7d6bbc98932fd19874ccb6a29
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-09 14:59:57 +0800

    prints filename in color and file lines

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'rainbow' # docs https://www.rubydoc.info/gems/rainbow/3.0.0
 
 # get input
 
@@ -17,4 +18,34 @@ if in_args[0] == 'ignore'
   scrap.each { |file| files.delete(file)  }
 end
 
-p files
+# p files
+
+# loop through files - after open
+for file in files
+  puts Rainbow(file).underline.bright
+  File.foreach(file) { |line| p line }
+end
+# next open and read file content - 1
+
+# no spaces if rule 
+
+# rule needs to be #rule {, .rule, html element { 
+# find rule simple
+
+# find rule html element
+
+# how to store loop through content
+
+# check indentation - may be possible with a string split
+
+# bonus: create live with upload version
+
+# if single line only 1 declaration
+
+# can even consider doing rule that 
+
+
+
+# check for properties - in assets folder - can also be loaded as script
+# https://gist.github.com/davidhund/3bd6757d6a36a283b0a2933666bd1976
+
