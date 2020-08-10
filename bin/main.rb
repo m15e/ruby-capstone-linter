2b4abdb504bd02be9f4190c5a576789db3a97f66
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-10 18:50:05 +0800

    Testing different ways of storing information

--- bin/main.rb
+++ bin/main.rb
@@ -2,12 +2,6 @@
 require 'rainbow' # docs https://www.rubydoc.info/gems/rainbow/3.0.0
 require 'json'
 
-html_tags_file = File.read('./assets/html-tags.json')
-
-html_tags = JSON.parse(html_tags_file)
-
-p html_tags.include?('div')
-
 
 # to be moved to lib 
 
@@ -23,10 +17,9 @@ class Lint
   end
 end
 
-# get input
+# get input files
 
 in_args = ARGV
-
 
 files = Dir.glob('**/**.css')  
 
@@ -42,22 +35,57 @@ end
 
 # p files
 
+
+def classify_line(line)
+  html_tags_file = File.read('./assets/html-tags.json')
+  html_tags = JSON.parse(html_tags_file)
+  css_props_file = File.read('./assets/css-properties.json')
+  css_props = JSON.parse(css_props_file)
+  
+  line_classes = ['space', 'eof', 'comment', 'rule', 'setting'] # consider adding pseudo_el
+  line_arr = line.split("/\s+/")  
+  full_line = line_arr[0]
+  split_line = line_arr[0].split(/ /)#end_of_line = line_arr[-1]
+
+  space_count = full_line.count(' ')
+  start_el = split_line[0]
+  end_el = split_line[-1]
+  line_class = ''
+  
+  #p tags    
+  if (html_tags.include? line_arr[0].split(' ')[0])
+    line_class = 'html_tag'
+  elsif line_arr[0].to_s.start_with?('.')
+    line_class = 'class_tag'
+  elsif line_arr[0].to_s.start_with?('#')
+    line_class = 'id_tag'  
+  elsif line_arr[0].to_s.start_with?('  ') and line_arr[0].split(' ')[0]
+    #p "!!!! DI #{line_arr[0].to_s} blop: #{line_arr[0].split(' ')}" # this still right??
+    line_class = 'double_indent'
+  elsif full_line == start_el and start_el == end_el 
+    line_class = 'empty line'
+    if start_el.start_with?("}\n")
+      line_class = 'close bracket'
+    end
+  end
+
+  #p "#{line_class}, #{start_of_line}, #{split_line} "
+  # line_class = (line_arr[0].to_s.start_with?'.') ? 'class_tag' : 'line_class_unknown'
+  # line_class = (line_arr[0].to_s.start_with?'#') ? 'id_tag' : 'line_class_unknown'
+  #if .include? line[0]  # simple case - consider first space is empty spaces
+  return [line_class, space_count, start_el, end_el, split_line]
+end
+
 # loop through files - after open
 for file in files
   lint = Lint.new
   lint.file_hash[:file_name] = file  
 
   puts Rainbow(file).underline.bright
-  File.foreach(file).with_index { |line, i| lint.file_hash[:lines] << [i, line] }
-  puts lint.file_hash  
+  File.foreach(file).with_index { |line, i| lint.file_hash[:lines] << [i+1, classify_line(line)] }
+  lint.file_hash[:lines].each { |l| p l }
 end
 
-def classify_line(line)
-  line_classes = ['space', 'eof', 'comment', 'rule', 'setting'] # consider adding pseudo_el
-
-  #if .include? line[0]  # simple case - consider first space is empty spaces
-  return line_class
-end
 
 # no spaces if rule 
 
---
+++ test.rb
@@ -0,0 +1,10 @@
+require 'json'
+
+# css_props_file = File.read('./assets/css-properties.json')
+# css_props = JSON.parse(css_props_file)
+
+# p css_props['property']
+
+p "Hello  there mr!".count(' ')
+
+p "Ru uu  by".index(' ')
