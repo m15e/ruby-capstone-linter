87a198d7897bd2337544358d5c81323d998228e0
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-11 23:53:18 +0800

    output functioning, checkpoint before refactor

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,13 @@ require 'json'
 html_tags_file = File.read('./assets/html-tags.json')
 $html_tags = JSON.parse(html_tags_file)
 css_props_file = File.read('./assets/css-properties.json')
-$css_props = JSON.parse(css_props_file)
+$css_props = Array.new
+css_props = JSON.parse(css_props_file).each { |pr| $css_props << pr.values[0] }
+
+# p $css_props.length
+$css_props = $css_props.uniq
+$selector_arr = ['html_tag', 'id_selector', 'class_selector']
+p $css_props.length, $css_props[..20]
 
 # to be moved to lib 
 
@@ -52,8 +58,8 @@ def first_el(line)
   space_split(line)[0].to_s
 end
 
-def last_el(line)
-  raw_split(line)
+def last_el_raw(line)
+  raw_split(line)[0]
 end
 
 def is_html_selector(line)
@@ -127,6 +133,15 @@ def has_newline(line)
   line.split("/\s+/")[-1].include?("\n")
 end
 
+def has_css_prop(line)
+  r = line.split(':')[0].strip
+  $css_props.include?(r) ? r : 'not-css-prop'
+end
+
+def is_single_line_rule(line) 
+  p line[-2]  
+end
+
 
 # loop through files - after open
 for file in files
@@ -134,10 +149,11 @@ for file in files
   lint.file_hash[:file_name] = file  
   line_count = `wc -l < #{file}`.to_i
   
+  
 
   puts Rainbow(file).underline.bright
-  puts "total lines: #{line_count}"
-  File.readlines(file).each_with_index { |line, i| lint.file_hash[:lines] << [i+1, classify_start(line), line.length, last_el(line), has_newline(line)] }
+  puts "total lines: #{line_count+1}"
+  File.readlines(file).each_with_index { |line, i| lint.file_hash[:lines] << [i+1, classify_start(line), line.length, last_el_raw(line), has_newline(line)] }
   
   
   #lc = File.open(file).readlines[-1]
@@ -153,21 +169,71 @@ for file in files
 
 
 
-  # loop through rules
+  # RULES
+
+  # find missing-end-of-source newline
   if last_line[-1] == false
-    puts "#{last_line[0]}:#{last_line[2]}  " + Rainbow("✖").red + "  Missing end-of-source newline"
-    
+    puts "#{last_line[0]}:#{last_line[2]} " + Rainbow("✖").red + " Missing end-of-source newline"    
   end
 
+  # create list of lines with double_indent 
+  lines_double_indent = lint.file_hash[:lines].select { |line| line[1] == 'double_indent' }
+  
+  lines_close_bracket = lint.file_hash[:lines].select { |line| line[-2].include? "}" }#line[1] == 'close_bracket' or }#(line[-2].include? "}") }
+  lines_open_bracket = lint.file_hash[:lines].select { |line| line[-2].include? "{" }
 
+  lines_rules = lint.file_hash[:lines].select { |line| $selector_arr.any?(line[1]) }
 
   
-  # development output
-  p "-"*50
-  p "Development output:"
-  lint.file_hash[:lines].each { |l| p l }
 
-  lint.file_hash[:lines].last[-1] == false
+  # check that css rule follows double_indent
+  lines_double_indent.each do |l| 
+    if has_css_prop(l[-2]) == 'not-css-prop'
+      puts "#{l[0]}:3 " + Rainbow("✖").red + " Expecting css-rule after double indent"
+    end
+  end
+
+  # css rule should end with ;
+  lines_double_indent.each do |l| 
+    line = l[-2]
+    if (has_css_prop(line) != 'not-css-prop') and (!(line).end_with?(";\n"))
+      puts "#{l[0]}:#{line.length} " + Rainbow("✖").red + " Expecting a trailing semicolon after setting CSS property."
+    end
+  end
+
+  # double indent before rule 
+
+  # no empty space after close bracket
+  lines_rules.each do |l|
+    line = l[-2]
+    if line.include?('{') and line.include?("}")
+      if line.end_with?(" \n")
+        puts "#{l[0]}:#{line.length} " + Rainbow("✖").red + " Missing new line after single line declaration."
+      end
+    end
+  end
+
+  # expecting newline after close bracket
+  lines_close_bracket.each do |l| 
+    if l[1] != 'close_bracket' 
+      puts "#{l[0]}:#{l[-2].length} " + Rainbow("✖").red + " Invalid close bracket, expecting \"}\" with no leading or trailing spaces. "
+      
+    end
+  end
+
+  puts "\n"
+
+
+
+  # expect new_line and opennign bracket after selector
+
+
+  # development output
+  # p "-"*50
+  # p "Development output:"
+  # lint.file_hash[:lines].each { |l| p l }
+
+  # lint.file_hash[:lines].last[-1] == false
 end
 
 
@@ -196,4 +262,3 @@ end
 
 # check for properties - in assets folder - can also be loaded as script
 # https://gist.github.com/davidhund/3bd6757d6a36a283b0a2933666bd1976
-
